9dcaa98b822aaf6a64e1f43ccc2889e4b2a70f11
NGUYENDINHHUNG <Nguyenhung21001@gmail.com>
2025-08-18 09:00:10 +0700

    feat/fix-login

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -2,21 +2,12 @@ import passport from "passport";
 import { Strategy as FacebookStrategy } from "passport-facebook";
 import { environment } from "./environment.js";
 
-const getCallbackURL = (provider) => {
-  const baseURL =
-    environment.NODE_ENV === "production"
-      ? process.env.SERVER_URL?.replace(/\/$/, '') // Loại bỏ dấu / ở cuối
-      : "http://localhost:8000";
-
-  return `${baseURL}/api/auth/${provider}/callback`;
-};
-
 passport.use(
   new FacebookStrategy(
     {
       clientID: environment.FACEBOOK_APP_ID,
       clientSecret: environment.FACEBOOK_APP_SECRET,
-      callbackURL: getCallbackURL("facebook"),
+      callbackURL: environment.FACEBOOK_CALLBACK_URL,
       profileFields: ["displayName", "photos", "name"],
     },
     async (accessToken, refreshToken, profile, cb) => {
